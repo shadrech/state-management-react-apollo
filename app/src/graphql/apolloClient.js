ce7a2b28b19aa94099625d5a8198a5699939c57a
Tatenda Chawanzwa <info@tatendachawanzwa.com>
2019-09-05 19:22:43 +0100

    Added isOpen to Worker

--- app/src/graphql/apolloClient.js
+++ app/src/graphql/apolloClient.js
@@ -1,5 +1,22 @@
 import ApolloClient from "apollo-boost";
+import fragment from "./queries/workerFragment.graphql";
 
 export default new ApolloClient({
-  uri: 'http://localhost:8000/graphql'
+  uri: 'http://localhost:8000/graphql',
+  clientState: {
+    resolvers: {
+      Worker: {
+        isOpen: () => false
+      },
+      Mutation: {
+        setWorkerOpen: (root, { id, isOpen }, { cache, getCacheKey }) => {
+          const cacheId = getCacheKey({ __typename: "Worker", id });
+          const worker = cache.readFragment({ fragment, id: cacheId });
+          const data = { ...worker, isOpen };
+          cache.writeData({ id: cacheId, data });
+          return null;
+        }
+      }
+    }
+  }
 });
